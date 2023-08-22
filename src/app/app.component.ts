0e34e526a8625fb73d239d9017522d6046ee6cc1
szilviszitar <szilviszitar@gmail.com>
2023-08-22 17:57:24 +0200

    pipes practise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pipes';
+
+  name = '';
+  date = '';
+  amount: number = 0;
+  heigth:number=0;
+  miles:number=0;
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
+
+  onHeigthChange(value:string){
+    this.heigth=parseFloat(value);
+  }
+
+  onMilesChange(value:string){
+    this.miles=parseFloat(value);
+  }
 }
